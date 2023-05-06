12821af2f916d85d2c4d16853ab557c527572bec
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-05-06 14:05:52 +0000

    openSUSE.

---
+++ src/opensuse/sh/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+zypper in -y shadow
+#zypper se --provides --match-exact chsh
+exit
--- src/opensuse/sh/install.sh
+++ src/opensuse/sh/install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 echo "Pre-install..."
+zypper addrepo -y https://download.opensuse.org/repositories/security/openSUSE_Tumbleweed/ security-x86_64
 echo "Installation stage 1..."
+zypper in -y ca-certificates
 echo "Installation stage 2..."
+zypper update -y
 echo "Installation stage 3..."
+zypper in -y systemd opendoas gpg2 bash zsh openssh-server \
+	unzip zip bzip2 lzip lziprecover brotli \
+	bind-utils net-tools iputils traceroute psmisc \
+	nano tree netcat-openbsd pv curl git screen htop neofetch
 echo "Post-install..."
 mkdir -p /run/sshd
 exit
