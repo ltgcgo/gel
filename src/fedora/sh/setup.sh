ac0dd8ee44a88b82bc23d00113cca2134a368951
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-08-05 06:53:37 +0000

    Fixed key press issues in Fedora.

--- src/fedora/sh/setup.sh
+++ src/fedora/sh/setup.sh
@@ -3,4 +3,8 @@ cd /root/sh/distro
 sed -i "s/__FLAVOUR__/Fedora/g" $PREFIX/etc/motd
 bash install.sh
 dnf clean dbcache
+echo "Applying ZSH key fix..."
+echo 'bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char' >> $PREFIX/etc/skel/.zshrc
 exit
