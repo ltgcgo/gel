f41fa3a3402597b7cced16b8cc720b3739aecbba
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-11-15 22:15:16 +0000

    Clean all cache generated by Fedora.

--- src/fedora/sh/setup.sh
+++ src/fedora/sh/setup.sh
@@ -2,7 +2,7 @@
 cd /root/sh/distro
 sed -i "s/__FLAVOUR__/Fedora/g" $PREFIX/etc/motd
 bash install.sh
-dnf clean dbcache
+dnf clean all
 echo "Applying ZSH key fix..."
 echo 'bindkey "^[[H" beginning-of-line
 bindkey "^[[F" end-of-line
