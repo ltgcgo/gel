106c22ff3310c6be23bd3be6a5bc5e03788756ba
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-11-21 22:54:36 +0000

    Do not change `root` passwords in native.

--- src/shared/sh/defaults.sh
+++ src/shared/sh/defaults.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Configure default root password
-echo "root:root" | chpasswd
+if [ "$MODE_NATIVE" != "1" ]; then
+	echo "root:root" | chpasswd
+fi
 # Point iptables to nftables
 ln -s /sbin/iptables-nft /sbin/iptables
 exit
