bea2afe933da13bbfd188599c13d8ee0b5a31de2
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-11-22 01:10:03 +0000

    Corrected man cleanup.

--- src/slimsuse/sh/cleanup.sh
+++ src/slimsuse/sh/cleanup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 ls -1 /usr/share/locale | while IFS= read -r entry; do
-	if [ -d "$entry" ]; then
+	if [[ "$entry" == *".alias" ]]; then
+		echo "Skipped $entry ."
+	else
 		echo "Removing $entry ..."
 		rm -rf "/usr/share/locale/$entry"
-	else
-		echo "Skipped $entry ."
 	fi
 done
 ls -1 /usr/share/man | while IFS= read -r entry; do
@@ -12,7 +12,7 @@ ls -1 /usr/share/man | while IFS= read -r entry; do
 		echo "Skipped $entry ."
 	else
 		echo "Removing $entry ..."
-		rm -rf "/usr/share/locale/$entry"
+		rm -rf "/usr/share/man/$entry"
 	fi
 done
 exit
