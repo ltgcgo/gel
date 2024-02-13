b9b70d00d42a66a34228bff4662d40e699c099c1
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-02-13 15:29:24 +0000

    Also apply the key fix for Alpine.

--- src/alpine/sh/setup.sh
+++ src/alpine/sh/setup.sh
@@ -2,4 +2,8 @@
 cd /root/gel/distro/sh
 sed -i "s/__FLAVOUR__/Alpine/g" $PREFIX/etc/motd
 bash install.sh
+echo "Applying ZSH key fix..."
+echo 'bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char' >> $PREFIX/etc/skel/.zshrc
 exit
