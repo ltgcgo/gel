443bd2bff759a50e856ea233fc02695701fc8642
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-07-01 13:55:02 +0000

    A slim version of Alpine Linux on Gel.

--- src/slimalp/sh/install.sh
+++ src/slimalp/sh/install.sh
@@ -4,8 +4,8 @@ apk upgrade
 echo "Installation stage 1..."
 apk add ca-certificates
 echo "Installation stage 2..."
-apk add openrc doas gpg bash zsh zsh-vcs openssh-server \
-	unzip zip tar bzip2 brotli xz \
+apk add openrc doas bash zsh zsh-vcs openssh-server \
+	unzip tar bzip2 brotli xz \
 	bind-tools net-tools iputils psmisc nftables coreutils \
 	nano tree netcat-openbsd pv
 echo "Post-install..."
--- src/slimalp/sh/setup.sh
+++ src/slimalp/sh/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /root/gel/distro/sh
-sed -i "s/__FLAVOUR__/Alpine/g" $PREFIX/etc/motd
+sed -i "s/__FLAVOUR__/Alpine Slim/g" $PREFIX/etc/motd
 bash install.sh
 bash cleanup.sh
+echo "Applying ZSH key fix..."
+echo 'bindkey "^[[H" beginning-of-line
+bindkey "^[[F" end-of-line
+bindkey "^[[3~" delete-char' >> $PREFIX/etc/skel/.zshrc
 exit
