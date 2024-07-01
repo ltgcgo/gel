401f361eca5949e447cc0181b7d6f2a45d58afcb
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-07-01 13:39:02 +0000

    Fixed a bug related to `sysctl` writes in OCI.

--- src/shared/sh/post.sh
+++ src/shared/sh/post.sh
@@ -4,6 +4,14 @@ bash syncSkel.sh
 rm -rf $PREFIX/var/log
 mkdir -p $PREFIX/var/log
 #rm -rf $PREFIX/root/sh
-sysctl -w net.core.default_qdisc=fq
-sysctl -w net.ipv4.tcp_congestion_control=bbr
+if [ "$MODE_NATIVE" != "" ]; then
+	echo "Customizing for native systems..."
+	sysctl -w net.core.default_qdisc=fq
+	sysctl -w net.ipv4.tcp_congestion_control=bbr
+	echo "Done."
+else
+	echo "Customizing for container systems..."
+	rm $PREFIX/etc/sysctl.d/netNative.conf
+	echo "Done."
+fi
 exit
