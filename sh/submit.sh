b3221d80d099e938a235985de2a33a3f97812c09
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-07-23 15:11:40 +0000

    Made failures not stopping subsequent builds.

--- sh/submit.sh
+++ sh/submit.sh
@@ -4,6 +4,9 @@ if [ -d "./dist/${1}/" ] ; then
 	podman build -t gel_${1}_latest .
 	podman push gel_${1}_latest docker://docker.io/${DH_USER:-ltgc}/gel:${1}
 	podman rmi gel_${1}_latest
+	if [ "$?" != "0" ]; then
+		echo "Image build failed. Attempting to skip the step..."
+	fi
 else
 	echo "Image \"${1}\" not found."
 fi
