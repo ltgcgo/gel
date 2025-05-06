10da9178e0483c9b6675bca02a15c818eff10466
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-05-06 09:37:37 +0000

    Slice creation with template succeeded.

--- src/host/scaffold.sh
+++ src/host/scaffold.sh
@@ -57,6 +57,14 @@ case $hostArch in
 		;;
 esac
 
+# In this file, slim Alpine containers are being created.
+if [ ! -f "gel.tlz" ]; then
+	curl -Lo "gel.tlz" "https://github.com/ltgcgo/gel/releases/latest/download/slimalp.tlz"
+fi
+if [ ! -f "gelInst.sh" ]; then
+	curl -Lo "gelInst.sh" "https://github.com/ltgcgo/gel/releases/latest/download/install.sh"
+fi
+
 # Fetch the LXC container subnet
 lxcSubNet="$(ip addr show lxcbr0 | grep "inet " | awk -F ' *|:' '/inet/{print $3}' | cut -d'/' -f1 | cut -d'.' -f1,2,3)"
 lxcSubNet=${lxcSubNet:-10.0.3}
@@ -68,30 +76,46 @@ names=( "web" "mix" "pod" "net" )
 # pod: Actual host for running Docker/Podman containers
 startOrder=1
 lxcTree="${PREFIX}/var/lib/lxc"
-#mkdir -p gel
-#cd gel
 
-# In this file, slim Alpine containers are being created.
-if [ ! -f "gel.tlz" ]; then
-	curl -Lo "gel.tlz" "https://github.com/ltgcgo/gel/releases/latest/download/slimalp.tlz"
-fi
-if [ ! -f "gelInst.sh" ]; then
-	curl -Lo "gelInst.sh" "https://github.com/ltgcgo/gel/releases/latest/download/install.sh"
-fi
+# Create LXC DHCP profile
+echo 'LXC_DHCP_CONFILE=/etc/lxc/dhcp.conf' >> "${PREFIX}/etc/default/lxc-net"
 
 # Create the base container to copy from
-lxc-create -t download -n base -- --dist alpine --release 3.21 --arch $targetArch
+lxc-create -t download -n alpbase -- --dist alpine --release 3.21 --arch $targetArch
 # Copy the setup files of Gel into the containers
-mkdir -p "${lxcTree}/base/rootfs/root/gel"
-cp -v gel.tlz "${lxcTree}/base/rootfs/root/gel/gel.tlz"
-cp -v gelInst.sh "${lxcTree}/base/rootfs/root/gel/install.sh"
+mkdir -p "${lxcTree}/alpbase/rootfs/root/gel"
+cp -v gel.tlz "${lxcTree}/alpbase/rootfs/root/gel/gel.tlz"
+cp -v gelInst.sh "${lxcTree}/alpbase/rootfs/root/gel/install.sh"
 # Start the container to begin configuration
-lxc-start -n base
-sleep 5s
-lxc-attach --clear-env -n base -u 0 -g 0 -v "GEL_SLIM=1" -- sh "/root/gel/install.sh"
-lxc-stop -n base
+lxc-start -n alpbase
+echo "Waiting for the container network..."
+sleep 4s
+lxc-attach -n alpbase -u 0 -g 0 -v "GEL_SLIM=1" -- sh "/root/gel/install.sh"
+lxc-stop -n alpbase
 
 # Create subsequent containers
+for name in ${names[@]}; do
+	printf "Configuring container: \"${name}\"..."
+	# Paths of the container
+	lxcPath="${lxcTree}/${name}"
+	lxcConf="${lxcPath}/config"
+	lxcRoot="${lxcPath}/rootfs"
+	# Create from base
+	lxc-copy -n alpbase -N "${name}"
+	# Configure autostart
+	echo -e "\nlxc.start.order = ${startOrder}\nlxc.start.auto = 1\nlxc.start.delay = 4" >> "${lxcConf}"
+	# Enable FUSE
+	echo -e "lxc.mount.entry = /dev/fuse dev/fuse none bind,create=file,rw 0 0" >> "${lxcConf}"
+	# Assign static IPv4 address
+	echo -e "dhcp-host=${name},10.0.3.$(($startOrder+2))" >> "${PREFIX}/etc/lxc/dhcp.conf"
+	# Assign static MAC addresses
+	echo -e "lxc.net.0.hwaddr = DE:ED:0E:A0:38:0${startOrder}" >> "${lxcConf}"
+	# Show the container name
+	echo "${name}" > "${lxcRoot}/etc/zsh/.customShellName"
+	# Increase the start order
+	startOrder=$(($startOrder+1))
+	echo " done."
+done
 
 # All done!
 exit
