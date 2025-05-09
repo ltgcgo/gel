ce1f0b2fd1456b5ffa3a2a5f6e333e5fee1d8167
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-05-09 18:29:54 +0000

    Automated Tor, I2Pd and Nyx install.

--- src/host/scaffold.sh
+++ src/host/scaffold.sh
@@ -70,11 +70,11 @@ lxcSubNet="$(ip addr show lxcbr0 | grep "inet " | awk -F ' *|:' '/inet/{print $3
 lxcSubNet=${lxcSubNet:-10.0.3}
 
 # Define names for the containers
-names=( "web" "mix" "pod" "net" )
+names=( "web" "mix" "net" "pod" )
 # web: Web servers, static files, etc
 # mix: Mixnet access, port forwarders for mixnet exposure, etc
 # pod: Actual host for running Docker/Podman containers
-startOrder=1
+startOrder=0
 lxcTree="${PREFIX}/var/lib/lxc"
 
 # Create LXC DHCP profile
@@ -92,6 +92,7 @@ echo "Waiting for the container network..."
 sleep 4s
 lxc-attach -n alpbase -u 0 -g 0 -v "GEL_SLIM=1" -- sh "/root/gel/install.sh"
 lxc-stop -n alpbase
+chmod 755 "${lxcTree}"
 
 # Create subsequent containers
 for name in ${names[@]}; do
@@ -103,18 +104,69 @@ for name in ${names[@]}; do
 	# Create from base
 	lxc-copy -n alpbase -N "${name}"
 	# Configure autostart
-	echo -e "\nlxc.start.order = ${startOrder}\nlxc.start.auto = 1\nlxc.start.delay = 4" >> "${lxcConf}"
+	echo -e "\nlxc.start.order = $(($startOrder+1))\nlxc.start.auto = 1\nlxc.start.delay = 4" >> "${lxcConf}"
 	# Enable FUSE
 	echo -e "lxc.mount.entry = /dev/fuse dev/fuse none bind,create=file,rw 0 0" >> "${lxcConf}"
-	# Assign static IPv4 address
-	echo -e "dhcp-host=${name},10.0.3.$(($startOrder+2))" >> "${PREFIX}/etc/lxc/dhcp.conf"
-	# Assign static MAC addresses
+	# Enable TUN
+	echo -e "lxc.mount.entry = /dev/net dev/net none bind,create=dir\nlxc.cgroup2.devices.allow = c 10:200 rwm" >> "${lxcConf}"
+	# Assign static IPv4 addresses
+	echo -e "dhcp-host=${name},10.0.3.$(($startOrder+3))" >> "${PREFIX}/etc/lxc/dhcp.conf"
+	# Assign static MAC addresses for static IPv6 addresses
 	echo -e "lxc.net.0.hwaddr = DE:ED:0E:A0:38:0${startOrder}" >> "${lxcConf}"
 	# Show the container name
 	echo "${name}" > "${lxcRoot}/etc/zsh/.customShellName"
+	# Configure subordinate IDs for unprivileged slices
+	subidConf="$((1048576+1048576*$startOrder))"
+	echo "root:${subidConf}:1048576" >> /etc/subuid
+	echo "root:${subidConf}:1048576" >> /etc/subgid
+	echo -e "lxc.include = /usr/share/lxc/config/userns.conf\nlxc.idmap = u 0 ${subidConf} 1048576\nlxc.idmap = g 0 ${subidConf} 1048576" >> "${lxcConf}"
+	chmod 755 "${lxcPath}"
+	chmod 755 "${lxcRoot}"
+	chmod 640 "${lxcConf}"
+	chown -R "${subidConf}:${subidConf}" "${lxcRoot}"
 	# Increase the start order
 	startOrder=$(($startOrder+1))
 	echo " done."
+done
+
+# Configuring "web"
+
+# Configuring "mix"
+echo -e "lxc.cgroup2.memory.max = 256M" >> "${lxcTree}/mix/config"
+echo -e "lxc.cgroup2.cpu.max = 200000 1000000" >> "${lxcTree}/mix/config"
+lxc-start -n "mix"
+lxc-attach -n "mix" -u 0 -- apk add tor nyx i2pd
+lxc-stop -n "mix"
+sed -i "s/ipv6 = false/ipv6 = true/g" "${lxcTree}/mix/rootfs/etc/i2pd/i2pd.conf"
+sed -i 's/# bandwidth = L/bandwidth = P/g' "${lxcTree}/mix/rootfs/etc/i2pd/i2pd.conf"
+sed -i "s/# address = 127.0.0.1/address = 10.0.3.5/g" "${lxcTree}/mix/rootfs/etc/i2pd/i2pd.conf"
+sed -i "s/# outproxy = http:\/\/false.i2p/outproxy = http:\/\/exit.stormycloud.i2p/g" "${lxcTree}/mix/rootfs/etc/i2pd/i2pd.conf"
+echo "ExcludeNodes {kp},{ir},{cu},{cn},{hk},{mo},{ru},{sy},{pk}" > "${lxcTree}/mix/rootfs/etc/tor/torrc"
+echo "StrictNodes 1" >> "${lxcTree}/mix/rootfs/etc/tor/torrc"
+echo "ClientUseIPv6 1" >> "${lxcTree}/mix/rootfs/etc/tor/torrc"
+echo "IPv6Exit 1" >> "${lxcTree}/mix/rootfs/etc/tor/torrc"
+echo "SocksListenAddress 10.0.3.5:9050" >> "${lxcTree}/mix/rootfs/etc/tor/torrc"
+echo "ControlListenAddress 10.0.3.5:9051" >> "${lxcTree}/mix/rootfs/etc/tor/torrc"
+
+# Configuring "pod"
+echo -e "lxc.include = /usr/share/lxc/config/nesting.conf" >> "${lxcTree}/pod/config"
+echo -e "lxc.cgroup2.cpu.max = 400000 1000000" >> "${lxcTree}/pod/config"
+echo -e "lxc.prlimit.nofile = 1048576" >> "${lxcTree}/pod/config"
+echo -e "user:524288:524288" >> "${lxcTree}/pod/rootfs/etc/subuid"
+echo -e "user:524288:524288" >> "${lxcTree}/pod/rootfs/etc/subgid"
+lxc-start -n "pod"
+lxc-attach -n "pod" -u 0 -- useradd -u 1000 -d /home/user -k /etc/skel -m -s "/bin/zsh"
+lxc-attach -n "pod" -u 0 -- apk add podman podman-compose
+#lxc-attach -n "pod" -u 1000 -- podman system migrate
+lxc-stop -n "pod"
+
+# Configuring "net"
+
+# Finishing touches
+for name in ${names[@]}; do
+	echo "Finished \"${name}\"."
+	#echo "Starting \"${name}\"..."
+	#lxc-start -n "${name}"
 done
 
 # All done!
