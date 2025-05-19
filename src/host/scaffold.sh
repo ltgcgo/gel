3054d3b16ce8be7ef75f460a2b50965c54b7c6d5
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2025-05-19 13:22:11 +0000

    Oops.

--- src/host/scaffold.sh
+++ src/host/scaffold.sh
@@ -137,7 +137,7 @@ done
 # Configuring "web"
 echo -e "lxc.cgroup2.memory.max = 384M" >> "${lxcTree}/web/config"
 lxc-start -n "web"
-sleep 4s
+sleep 2s
 lxc-attach -n "web" -u 0 -- apk add caddy coredns haproxy
 lxc-attach -n "web" -u 0 -- systemctl disable sshd
 lxc-attach -n "web" -u 0 -- systemctl enable caddy
@@ -149,7 +149,7 @@ lxc-stop -n "web"
 echo -e "lxc.cgroup2.memory.max = 256M" >> "${lxcTree}/mix/config"
 echo -e "lxc.cgroup2.cpu.max = 200000 1000000" >> "${lxcTree}/mix/config"
 lxc-start -n "mix"
-sleep 4s
+sleep 2s
 lxc-attach -n "mix" -u 0 -- bash -c 'mkdir -p /lib/modules/$(uname -r)'
 lxc-attach -n "mix" -u 0 -- apk add tor nyx i2pd yggdrasil
 lxc-attach -n "mix" -u 0 -- systemctl disable sshd
@@ -181,10 +181,11 @@ echo -e "lxc.mount.entry = sys dev/.lxc/sys sysfs create=dir,optional 0 0" >> "$
 #echo -e "lxc.include = /usr/share/lxc/config/nesting.conf" >> "${lxcTree}/pod/config"
 echo -e "lxc.cgroup2.cpu.max = 400000 1000000" >> "${lxcTree}/pod/config"
 echo -e "lxc.prlimit.nofile = 1048576" >> "${lxcTree}/pod/config"
+lxc-start -n "pod"
+sleep 2s
 lxc-attach -n "pod" -u 0 -- bash -c 'echo -e "user:131074:524288" >> "/etc/subuid"'
 lxc-attach -n "pod" -u 0 -- bash -c 'echo -e "user:131074:524288" >> "/etc/subgid"'
 lxc-attach -n "pod" -u 0 -- bash -c 'echo -e "#!/sbin/openrc-run\n\ndescription=\"Garbage disposal\"\n\ncommand=\"/bin/rm\"\ncommand_args=\"-rf /tmp/*\"\ncommand_background=true\npidfile=\"/run/$RC_SVCNAME.pid\"" > "/etc/init.d/tmp-clean"'
-lxc-start -n "pod"
 sleep 4s
 lxc-attach -n "pod" -u 0 -- apk add podman podman-compose
 lxc-attach -n "pod" -u 0 -- chmod +x /etc/init.d/tmp-clean
